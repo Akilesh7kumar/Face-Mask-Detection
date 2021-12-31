9ab032465d43ac2d4a9466e3581476e533e07e1f
AkileshKumar <56359926+Akilesh7kumar@users.noreply.github.com>
2021-12-31 12:06:00 +0530

    Create Face mask detection.py

---
+++ Face mask detection.py
@@ -0,0 +1,96 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow import keras
+
+test_dr= r'D:\DSA Java\Deep_learning_internshala\Final_Assignment_Face Mask Dataset\Test'
+validation_dr=r'D:\DSA Java\Deep_learning_internshala\Final_Assignment_Face Mask Dataset\Validation'
+train_dr=r'D:\DSA Java\Deep_learning_internshala\Final_Assignment_Face Mask Dataset\Train'
+ from tensorflow.keras.preprocessing.image import ImageDataGenerator
+ #Generating batches of tensor image data
+train_datagen = ImageDataGenerator(rescale = 1./255)
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+train_generator = train_datagen.flow_from_directory(
+             train_dr,
+             target_size=(128,128),
+             batch_size=20,
+             class_mode='binary')
+
+
+validation_generator = test_datagen.flow_from_directory(
+             validation_dr,
+             target_size=(128,128),
+             batch_size=20,
+             class_mode='binary')
+             
+ from tensorflow.keras import layers
+ from tensorflow.keras import models   
+ model = models.Sequential()
+
+model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                        input_shape=(128, 128, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Flatten())
+
+model.add(layers.Dense(512, activation='relu'))
+
+model.add(layers.Dense(1, activation='sigmoid'))
+model.summary()
+
+from tensorflow.keras import optimizers
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+              
+pd.DataFrame(history.history).plot(figsize=(8,5))
+plt.grid(True)
+plt.gca().set_ylim(0,1)
+plt.show()
+
+from tensorflow.keras import models
+from tensorflow.keras import layers
+
+model = models.Sequential()
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+from tensorflow.keras import optimizers
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=2e-5),
+              metrics=['acc'])
+              
+checkpoint_cb = keras.callbacks.ModelCheckpoint("CNN_Project_Model-{epoch:02d}.h5")
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=100,
+      epochs=5,
+      validation_data=validation_generator,
+      validation_steps=50,
+      callbacks=[checkpoint_cb])
+ pd.DataFrame(history.history).plot(figsize=(8, 5))
+plt.grid(True)
+plt.gca().set_ylim(0, 1)
+plt.show()
+hist_df = pd.DataFrame(history.history) 
+test_generator = test_datagen.flow_from_directory(
+        test_dir,
+        target_size=(128, 128),
+        batch_size=20,
+        class_mode='binary')
+model.evaluate_generator(test_generator, steps=50)           
